dc8eba06914e91e12991a73184c73d4f0d0f1bc9
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2018-03-24 05:46:18 +0900

    fix layout of location detail page

--- src/Location.js
+++ src/Location.js
@@ -6,8 +6,10 @@ import {
   View,
   TouchableOpacity,
   TextInput,
-  Image
+  Image,
+  ScrollView
 } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
 import {
   Actions
 } from 'react-native-router-flux'
@@ -16,24 +18,80 @@ export default class Location extends Component {
 
   render () {
     return(
-      <View>
-        <Text>
-          {this.props.location.title}
-        </Text>
-        <Text>
-          {this.props.location.content}
-        </Text>
-        <Text>
-          {this.props.location.latitude}
-        </Text>
-        <Text>
-          {this.props.location.longitude}
-        </Text>
-        <Image
-          source={{ uri: 'data:image/jpeg;base64,' + this.props.location.image }}
-          style={{ width: 400, height: 400 }}
-        />
-      </View>
+      <ScrollView style={styles.container}>
+        <View style={styles.title}>
+          <Text style={{ fontSize: 16}}>
+            {this.props.location.title}
+          </Text>
+        </View>
+        <View style={styles.title}>
+          <Text>
+            {this.props.location.content}
+          </Text>
+        </View>
+        <View style={styles.imageContainer}>
+          <Image
+            source={{ uri: 'data:image/jpeg;base64,' + this.props.location.image }}
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.mapContainer}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: this.props.location.latitude,
+              longitude: this.props.location.longitude,
+              latitudeDelta: 0.02,
+              longitudeDelta: 0.02,
+            }}
+            showsUserLocation={true}
+          >
+            <Marker
+              coordinate={{ latitude: this.props.location.latitude, longitude: this.props.location.longitude }}
+              centerOffset={{ x: 0, y: -35 }}
+            >
+              <Image
+                source={{ uri: 'data:image/jpeg;base64,' + this.props.location.image }}
+                style={{ width: 48, height: 48, borderRadius: 24, position: 'absolute', top: 4, left: 4 }}
+              />
+              <Image
+                source={require('../images/marker_icon.png')}
+                style={{ width: 56, height: 70 }}
+              />
+            </Marker>
+          </MapView>
+        </View>
+      </ScrollView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 30,
+  },
+  title: {
+    marginBottom: 20,
+  },
+  content: {
+    marginBottom: 20
+  },
+  imageContainer: {
+    alignItems: 'center',
+    marginBottom: 20
+  },
+  image: {
+    height: 300,
+    width: 300
+  },
+  mapContainer: {
+    alignItems: 'center',
+    marginBottom: 100
+  },
+  map: {
+    height: 300,
+    width: 300
+  }
+})
