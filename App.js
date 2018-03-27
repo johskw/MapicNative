d7b0afacc4505ad3d881ee6e0c9cc78949559f80
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2018-03-28 00:55:55 +0900

    add icons to tabbar

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {
   StyleSheet,
+  Text,
   AsyncStorage
 } from 'react-native'
 import {
@@ -16,6 +17,24 @@ import LoginForm from './src/LoginForm'
 import SignupForm from './src/SignupForm'
 import MyLocations from './src/MyLocations'
 import DrawerContent from './src/DrawerContent'
+
+class MapIcon extends Component {
+  render () {
+    let iconColor = this.props.focused ? '#ffa500' : '#888'
+    return(
+      <Text style={{ fontFamily: 'fontawesome', fontSize: 24, color: iconColor }}>&#xf041;</Text>
+    )
+  }
+}
+
+class UserIcon extends Component {
+  render () {
+    let iconColor = this.props.focused ? '#ffa500' : '#888'
+    return(
+      <Text style={{ fontFamily: 'fontawesome', fontSize: 24, color: iconColor }}>&#xf007;</Text>
+    )
+  }
+}
 
 export default class App extends Component {
 
@@ -40,13 +59,14 @@ export default class App extends Component {
               key="tabbar"
               tabs
               tabBarStyle={styles.tabBar}
+              labelStyle={{ color: this.props.focused ? '#ffa500' : '#888' }}
             >
-              <Stack title="MAP">
+              <Stack key="MAP" icon={MapIcon} >
                 <Scene key="map" component={Map} title="MAP" apiUrl={apiUrl} />
                 <Scene key="newLocation" component={LocationForm} title="新規投稿" apiUrl={apiUrl} />
                 <Scene key="Location" component={Location} title="投稿" />
               </Stack>
-              <Stack title="自分の投稿">
+              <Stack key="自分の投稿" icon={UserIcon} >
                 <Scene key="MyLocations" component={MyLocations} title="自分の投稿" apiUrl={apiUrl} />
                 <Scene key="myLocation" component={Location} title="投稿" />
               </Stack>
@@ -65,4 +85,7 @@ const styles = StyleSheet.create({
   navBarTitle: {
     color: '#000'
   },
+  tabBar: {
+    backgroundColor: '#fff'
+  }
 })
