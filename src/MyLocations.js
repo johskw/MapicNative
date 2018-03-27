a320d254ec771d168a03f09305727faf71df82de
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2018-03-27 19:19:06 +0900

    add map top my location page

--- src/MyLocations.js
+++ src/MyLocations.js
@@ -20,7 +20,8 @@ export default class Location extends Component {
     super(props)
     this.state = {
       user: {},
-      locations: []
+      locations: [],
+      displayMap: false
     }
   }
 
@@ -61,7 +62,13 @@ export default class Location extends Component {
       })
   }
 
-  render() {
+  changeDisplayType () {
+    this.setState({
+      displayMap: !this.state.displayMap
+    })
+  }
+
+  renderImageList () {
     const imageList = this.state.locations.map((location, i) => (
       <TouchableOpacity
         key={i + 1}
@@ -75,14 +82,74 @@ export default class Location extends Component {
     ))
 
     return (
-      <ScrollView style={styles.container}>
-        <View style={styles.imageListContainer}>
+      <View style={styles.container}>
+        <View style={styles.changeBtnContainer}>
+          <TouchableOpacity
+            onPress={() => this.changeDisplayType()}
+            style={styles.changeBtn}
+          >
+            <Text style={styles.changeBtnText}>MAPで表示</Text>
+          </TouchableOpacity>
+        </View>
+        <ScrollView style={styles.imageListContainer}>
           <View style={styles.imageList}>
             {imageList}
           </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </View>
     )
+  }
+
+  renderMap () {
+    const markerList = this.state.locations.map((location, i) => (
+      <Marker
+        key={i + 1}
+        coordinate={{ latitude: location.latitude, longitude: location.longitude }}
+        onPress={() => { Actions.myLocation({ location: location }) }}
+        centerOffset={{ x: 0, y: -35 }}
+      >
+        <Image
+          source={{ uri: 'data:image/jpeg;base64,' + location.image }}
+          style={{ width: 48, height: 48, borderRadius: 24, position: 'absolute', top: 4, left: 4 }}
+        />
+        <Image
+          source={require('../images/marker_icon.png')}
+          style={{ width: 56, height: 70 }}
+        />
+      </Marker>
+    ))
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.changeBtnContainer}>
+          <TouchableOpacity
+            onPress={() => this.changeDisplayType()}
+            style={styles.changeBtn}
+          >
+            <Text style={styles.changeBtnText}>画像一覧</Text>
+          </TouchableOpacity>
+        </View>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 35.681167,
+            longitude: 139.767052,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+        >
+          {markerList}
+        </MapView>
+      </View>
+    )
+  }
+
+  render () {
+    if (this.state.displayMap) {
+      return this.renderMap()
+    }
+    return this.renderImageList()
   }
 }
 
@@ -91,15 +158,28 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#eee',
   },
-  mainContainer: {
-    backgroundColor: '#fff',
-    paddingTop: 0,
-    padding: 30,
+  changeBtnContainer: {
+    position: 'absolute',
+    top: 10,
+    right: 20,
+    zIndex: 10
+  },
+  changeBtn: {
+    backgroundColor: '#ffa500',
+    borderRadius: 15,
+    width: 100,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.2,
+    shadowOpacity: 0.2
+  },
+  changeBtnText: {
+    color: '#fff'
   },
   imageListContainer: {
+    paddingTop: 50
   },
   imageList: {
     flexDirection: 'row',
@@ -108,5 +188,6 @@ const styles = StyleSheet.create({
   image: {
     width: 125,
     height: 125
-  }
+  },
+  map: { ...StyleSheet.absoluteFillObject, }
 })
